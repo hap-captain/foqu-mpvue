6c7eb002617fda33bf6c1f222fdec1c2f995d9e1
hap-captain <3187138761@qq.com>
2022-04-11 23:31:42 +0800

    设置bar

---
+++ src/pages/message/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="">
+    hello world
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/pages/square/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="">
+    square
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
